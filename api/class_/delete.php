eaf8440f77a672f43efb714f83a76591a9ec35d7
dihamza <62916332+dihamza@users.noreply.github.com>
2021-07-06 20:10:51 +0000

    Add files via upload

--- api/class_/delete.php
+++ api/class_/delete.php
@@ -1,9 +1,7 @@
 <?php
-
 // required headers
 header("Access-Control-Allow-Origin: * ");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
 
 //require student class to instantiate an object
 require_once '../../models/class_.php';
@@ -15,36 +13,30 @@ require_once '../../config/Database.php';
 $database = new Database();
 $conn = $database->getConnection();
 
-
-// instantiate product object
+//get subject instance here
 $class = new Class_($conn);
 
-// get posted data
-
+//posted name to delete
 $data = json_decode(file_get_contents("php://input"), TRUE);
-$class->name = $data['name'];
+$toDelete = $data['name'];
 
-//use the create() method here
-// create the admin
-if(
-    !empty($class->name) &&
-    $class->create()
-){
+// use the delete() method here
+// delete subject
+if($class->delete($toDelete)){
 
     // set response code
     http_response_code(200);
 
-    // display message: admin was created
-    echo json_encode(array("message" => "Class was created."));
+    // display message: subject was deleted
+    echo json_encode(array("message" => "class was Deleted."));
 }
 
-// message if unable to create admin
+// message if unable to delete subject
 else{
 
     // set response code
     http_response_code(400);
 
-    // display message: unable to create admin
-    echo json_encode(array("message" => "Unable to create Class."));
+    // display message: unable to delete subject
+    echo json_encode(array("message" => "Unable to delete the class."));
 }
-?>
--- api/class_/update.php
+++ api/class_/update.php
@@ -1,11 +1,9 @@
 <?php
-
 // required headers
 header("Access-Control-Allow-Origin: * ");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
 
-//require student class to instantiate an object
+//require subject class to instantiate objects
 require_once '../../models/class_.php';
 
 //require Database class to get connect to database
@@ -15,36 +13,37 @@ require_once '../../config/Database.php';
 $database = new Database();
 $conn = $database->getConnection();
 
-
-// instantiate product object
+//get subject instance here
 $class = new Class_($conn);
 
-// get posted data
-
+//prepare posted data
 $data = json_decode(file_get_contents("php://input"), TRUE);
+
+$toUpdate = $data['nameAmod'];
 $class->name = $data['name'];
 
-//use the create() method here
-// create the admin
+// use the update() method here
+// update the subject
 if(
     !empty($class->name) &&
-    $class->create()
+    !empty($toUpdate) &&
+    $class->update($toUpdate)
 ){
 
     // set response code
     http_response_code(200);
 
-    // display message: admin was created
-    echo json_encode(array("message" => "Class was created."));
+    // display message: Subject was updated.
+    echo json_encode(array("message" => "Class was updated."));
 }
 
-// message if unable to create admin
+// message if unable to update subject
 else{
 
     // set response code
     http_response_code(400);
 
-    // display message: unable to create admin
-    echo json_encode(array("message" => "Unable to create Class."));
+    // display message: Unable to update Subject.
+    echo json_encode(array("message" => "Unable to update class."));
 }
-?>
+
